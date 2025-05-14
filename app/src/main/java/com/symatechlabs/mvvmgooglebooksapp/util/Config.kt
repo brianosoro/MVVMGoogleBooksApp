d3a81500a03f091ada4e97b062996dc11a0ddbf4
Brian Osoro <info@brianosoro.com>
2025-05-14 22:53:59 +0300

    Starting....

---
+++ app/src/main/java/com/symatechlabs/mvvmgooglebooksapp/util/Config.kt
@@ -0,0 +1,23 @@
+package com.symatechlabs.mvvmgooglebooksapp.util
+
+object Config {
+
+        const val RESULT_CODE = 10005
+        const val LOG_TAG = "MVVM_GOOGLE_BOOKS_APP";
+        const val BASE_URL = "https://www.googleapis.com/";
+        const val VAL_01_INTENT_VALUE = "val_01";
+        const val VAL_02_INTENT_VALUE = "val_02";
+        const val VAL_03_INTENT_VALUE = "val_03";
+        const val VAL_04_INTENT_VALUE = "val_04";
+        const val VAL_05_INTENT_VALUE = "val_05";
+        const val VAL_06_INTENT_VALUE = "val_06";
+        const val VAL_07_INTENT_VALUE = "val_07";
+        const val VAL_08_INTENT_VALUE = "val_08";
+        const val VAL_09_INTENT_VALUE = "val_09";
+        const val GENERAL_ERROR = "An error occurred handling your request. Please retry.";
+        const val NO_EMAIL_CLIENT = "You seem not to have an email client installed.";
+        const val APP_URL = "https://rebrand.ly/azoti8z"
+        const val WEBSITE_URL = "https://rebrand.ly/f509ac"
+        const val TWITTER_URL = "https://rebrand.ly/8edd84"
+
+}
